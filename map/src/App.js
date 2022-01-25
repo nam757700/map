b11f502558842f95133e554ba4b17df9a5485885
nam <donam3005@gmail.com>
2022-01-25 16:24:30 +0700

    abc

---
+++ map/src/App.js
@@ -0,0 +1,12 @@
+import "./index";
+import Mapbox from "../src/map/mapbox.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <Mapbox />
+    </div>
+  );
+}
